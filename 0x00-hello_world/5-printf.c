da69a47c3f8a1a17318927466445a6e5ab2f25c8
Koech Roy <koechroy06@gmail.com>
2022-06-02 23:46:30 +0000

    low level

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - prints out a string using printf function
+ *
+ * Description: the program prints out a simple array of characters
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
