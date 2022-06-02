223ccdac418775a0dad51c534272909fff9f5868
Koech Roy <koechroy06@gmail.com>
2022-06-02 23:28:21 +0000

    low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 
-/* main - the programs entry point */
+/**
+ *  main - the programs entry point
+ *  using puts function to utput an array of characters 
+ */
 
 int main(void)
 {
 
-	/* using puts function to utput an array of characters */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
