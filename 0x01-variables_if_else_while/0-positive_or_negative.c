36d273cd7255971235fec87a173774478da89cdb
Koech Roy <koechroy06@gmail.com>
2022-06-03 14:28:54 +0000

    variables,if,else,while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,31 +5,31 @@
 /**
  * main - entry point
  * Description: this function determines if the randomly generated n
- * is a number less than equal to or greater than zero 
- * Return: 0 
+ * is a number less than equal to or greater than zero
+ * Return: 0
  */
 
 int main(void)
 {
 	int n;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if(n > 0)
+	if( n > 0)
 	{
 	
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 
 	}
-	if(n == 0)
+	if( n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	
 	}
-	if(n < 0)
+	if( n < 0)
 	{
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 	}
 
 	return (0);
