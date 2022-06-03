9b2df5e1e41533780a1ef0d70d9642ffcfd3d699
Koech Roy <koechroy06@gmail.com>
2022-06-03 22:05:42 +0000

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - entry block
+  * Return: 0
+*/
+
+int main(void)
+{
+	char z;
+
+	for (z = 'z' ; z >= 'a'; z--)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
