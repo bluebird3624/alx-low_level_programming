9b99807edc95d0bfbf130b772108fd88be879c90
Koech Roy <koechroy06@gmail.com>
2022-06-03 21:50:45 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - entry block
+* description: prints single digit numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
