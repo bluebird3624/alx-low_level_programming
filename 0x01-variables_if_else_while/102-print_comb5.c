12217d5c92f8cb7bdc3c4a401e694462a5dcfa60
Koech Roy <koechroy06@gmail.com>
2022-06-06 13:02:44 +0000

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,17 +9,34 @@
 
 int main(void)
 {
-    int num1,num2;
+    int num1,num2,num3,num4;
 
     for(num1 = 48; num1 <= 57; num1++)
     {
         for(num2 = 48; num2 <= 57; num2++)
         {
-            putchar(num1);
-            putchar(num2);
-            putchar(',');
-            putchar(' ');
+            
+
+            for(num3 = 48; num3 <= 57; num3++)
+            {
+                for(num4 = 48; num4 <= 57; num4++)
+                {
+                    if (num1 == num3 && num2 == num4)
+                    {
+                        continue;
+                    }
+                    putchar(num1);
+                    putchar(num2);
+                    putchar(' ');
+                    putchar(num3);
+                    putchar(num4);
+                    putchar(',');
+                }
+            }
+
         }
     }
+
+    
     return (0);
 }
