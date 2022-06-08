c5053eaed8b268cd12bde59e9d8a68da9767c6b8
Koech Roy <koechroy06@gmail.com>
2022-06-08 22:58:43 +0000

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,20 +6,35 @@
  * @n: the number input as function parameter
  */
 
+void print(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+
+	if (n/10)
+	{
+		print(n/10);
+		putchar(n%10 + '0');
+}
+
+
 void print_to_98(int n)
 {
 	if (n > 98)
 	{
 		for (n = n; n >= 98; n--)
 		{
-			_putchar(n + '0');
+			print(n);
 		}
 	}
 	else
 	{
 		for (n = n; n <= 98; n++)
 		{
-			_putchar(n + '0');
+			print(n);
 		}
 	}
 	_putchar('\n');
