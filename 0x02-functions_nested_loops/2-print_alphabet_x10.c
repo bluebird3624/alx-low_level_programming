f9f666c931350a794710eb5f2836067657bfe44a
Koech Roy <koechroy06@gmail.com>
2022-06-08 02:38:10 +0000

    alphabates*10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * alphabetx10 - print alphabets 10 times
+ * Return: 0
+ */
+void alphabetx10(void)
+{
+	char a, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 'a'; c <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
