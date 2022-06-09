f7628fbb6db27df6cbd50fb123883d58e3c09ac6
Koech Roy <koechroy06@gmail.com>
2022-06-09 14:16:05 +0000

    debugging

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isupper - determines if c is a lower case or upper case
+ * @c: contains an integer value set by main
+ * Return: 1 for uppercase, 0 for otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
