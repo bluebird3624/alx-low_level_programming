7b3c8d8f8c5f54b9700f0110043c7013e75eb580
Koech Roy <koechroy06@gmail.com>
2022-06-13 17:10:58 +0000

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* _puts - prints out a string and a newline
+* @str: pointer to the location of passed in value
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
