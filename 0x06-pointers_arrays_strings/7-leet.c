ef2d5ffc0484da174048ecdabf938a65fbc89cc1
Koech Roy <koechroy06@gmail.com>
2022-06-16 11:26:13 +0000

    Mozart composed his music not for the elite

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string to leet
+ * @s: input
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+
+	char b[] = "ol_ea__t";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; b[j] != '\0'; j++)
+		{
+			if (s[i] == b[j] || s[i] == (b[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+	}
+	return (s);
+}
