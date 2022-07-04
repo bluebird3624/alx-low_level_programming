64f5aaa749165a7a4d70fc6588e9f454ae639d17
Koech Roy <koechroy06@gmail.com>
2022-07-04 23:10:07 +0000

    Write a program that prints the name of the file it was compiled from, followed by a new line.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - printing the name of the compiled file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
 {
-	printf("%s \n",argv[0]);
+	printf("%s \n", argv[0]);
+
+	return (0);
 }
