46b0fd0fe92b509a5dae8b60dce64aeceb900542
Koech Roy <koechroy06@gmail.com>
2022-07-21 22:27:38 +0000

    Write a function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,52 @@
 #include "main.h"
 
+/**
+ * exponent - a function that calculates the exponent of a numbers based 2
+ * @expo: the power of the base 2
+ * Return: the exponent of base two to the power passed in
+ */
+
 int exponent(int expo)
 {
 	int i;
 	int ans = 2;
-	if(expo == 0)
+
+	if (expo == 0)
 	{
 		return (1);
 	}
-	else if(expo == 1)
+	else if (expo == 1)
 	{
 		return (2);
 	}
 	else
 	{
-		for(i = 1; i < expo; i++)
+		for (i = 1; i < expo; i++)
 		{
-			ans = ans *2;
+			ans = ans * 2;
 		}
 		return (ans);
 	}
 }
 
+/**
+ * binary_to_uint - function that converts a binary string to base 10 numbers
+ * @b: the binary string passed to the function
+ * Return: the converted number or zero if the binary
+ * string contains elements other then 0,1 an the null byte
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int count;
-	for(count = 0; string[count] != '\0'; count++)
+
+	for (count = 0; string[count] != '\0'; count++)
 	{
-		if(string[count] != '0')
+		if (string[count] != '0')
 		{
-			if(string[count] != '1')
+			if (string[count] != '1')
 			{
-				if(string[count] != '\0')
+				if (string[count] != '\0')
 				{
 					return (0);
 				}
@@ -43,9 +58,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int position = 0;
 	unsigned int ans = 0;
 
-	for(rev = (count - 1); rev >= 0; rev--)
+	for (rev = (count - 1); rev >= 0; rev--)
 	{
-		if(string[rev] == '1')
+		if (string[rev] == '1')
 		{
 			ans = ans + exponent(position);
 		}
