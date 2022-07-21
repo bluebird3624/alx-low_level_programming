da9f267c8c9dd8f3ad542d1d76873463f143a19c
Koech Roy <koechroy06@gmail.com>
2022-07-21 22:46:01 +0000

    Write a function that converts a binary number to an unsigned int.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -39,14 +39,17 @@ int exponent(int expo)
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int count;
+	int rev;
+	unsigned int position = 0;
+	unsigned int ans = 0;
 
-	for (count = 0; string[count] != '\0'; count++)
+	for (count = 0; b[count] != '\0'; count++)
 	{
-		if (string[count] != '0')
+		if (b[count] != '0')
 		{
-			if (string[count] != '1')
+			if (b[count] != '1')
 			{
-				if (string[count] != '\0')
+				if (b[count] != '\0')
 				{
 					return (0);
 				}
@@ -54,13 +57,9 @@ unsigned int binary_to_uint(const char *b)
 		}
 	}
 
-	int rev;
-	unsigned int position = 0;
-	unsigned int ans = 0;
-
 	for (rev = (count - 1); rev >= 0; rev--)
 	{
-		if (string[rev] == '1')
+		if (b[rev] == '1')
 		{
 			ans = ans + exponent(position);
 		}
